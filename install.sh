97089079086ac6a541bad7b38b7ed2ae0d9c2ee3
natureyoo <jayeon.yoo@snu.ac.kr>
2020-07-07 22:37:52 +0900

    feat: necessary packages

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
-git clone -b cud102_tor15_p --single-branch https://github.com/natureyoo/detectron2.git
-python -m pip install -e detectron2
 pip install -r ./requirements.txt
+git clone -b cud102_tor15_p --single-branch https://github.com/natureyoo/detectron2.git ~/detectron2
+cd ~/
+python -m pip install -e detectron2
